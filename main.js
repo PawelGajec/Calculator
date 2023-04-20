baeff558eb5f93665840737ae31cec66b4535c23
Paweł Gajec <103367076+PawelGajec@users.noreply.github.com>
2023-04-20 18:41:15 +0200

    func: improve calculation methods

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for (let key of keys) {
         }
         else if (value == 'backspace') {
             input = input.slice(0, -1);
-            display_input.innerHTML = input;
+            display_input.innerHTML = cleanInput(input);
         }
         else if (value == '=') {
             let result = eval(input);
@@ -39,10 +39,48 @@ for (let key of keys) {
              ){
                 input += ')';
             }
-            display_input.innerHTML = input;
+            display_input.innerHTML = cleanInput(input);
         } else {
             input += value;
-            display_input.innerHTML = input;
+            display_input.innerHTML = cleanInput(input);
         }
     })
+}
+
+function cleanInput(input) {
+    let input_array = input.split('');
+    let input_array_length = input_array.length;
+
+    for (let i = 0; i < input_array_length; i++ ){
+        if(input_array[i] == '*') {
+            input_array[i] = `<span class='operator'>*</span>`;
+        }else if (input_array[i] == '/') {
+            input_array[i] = `<span class='operator'>/</span>`;
+        }else if (input_array[i] == '+') {
+            input_array[i] = `<span class='operator'>+</span>`;
+        }else if (input_array[i] == '-') {
+            input_array[i] = `<span class='operator'>-</span>`;
+        }else if (input_array[i] == '(') {
+            input_array[i] = `<span class='brackets'>(</span>`;
+        }else if (input_array[i] == ')') {
+            input_array[i] = `<span class='brackets'>)</span>`;
+        }else if (input_array[i] == '%') {
+            input_array[i] = `<span class='percent'>%</span>`;
+        }
+    }
+    return input_array.join('');
+}
+
+function cleanOutput (output) {
+    let output_string = output.toString();
+    let decimal = output_string.split('.')[1];
+    output_string = output_string.split('.')[0];
+
+    let output_array = output_string.split('');
+
+    if (output_array.length > 3 ) {
+        for (let i=output_array.length -3; i > 0; i -= 3) {
+            
+        }
+    }
 }
